204bd1a35e42bf06d0f25117c661b1af2d129c0a
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-03-15 15:18:43 +0800

    GFG-Amazon 15/3/2016#2

--- java-based/src/geeksforgeeks/company/amazon/LargestSquare.java
+++ java-based/src/geeksforgeeks/company/amazon/LargestSquare.java
@@ -5,6 +5,45 @@
 
 package geeksforgeeks.company.amazon;
 
+import java.util.Scanner;
+
 public class LargestSquare {
+	static int[][] grid = null;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+
+		grid = new int[r][c];
+		for (int i = 0; i < r; i++)
+			for (int j = 0; j < c; j++)
+				grid[i][j] = sc.nextInt();
+		System.out.println(maxSquare());
+	}
+
+	static int maxSquare() {
+		int[][] maxmat = new int[grid.length][grid[0].length];
+		for (int i = 0; i < grid.length; i++)
+			maxmat[i][0] = grid[i][0];
+		for (int j = 0; j < grid[0].length; j++)
+			maxmat[0][j] = grid[0][j];
+
+		int maxSq = 0;
+
+		for (int i = 1; i < grid.length; i++) {
+			for (int j = 1; j < grid[0].length; j++) {
+				if (grid[i][j] == 1) {
+					int min = Math.min(maxmat[i - 1][j], maxmat[i][j - 1]);
+					min = Math.min(min, maxmat[i - 1][j - 1]);
+					maxmat[i][j] = min + 1;
+				} else
+					maxmat[i][j] = 0;
+
+				maxSq = maxSq < maxmat[i][j] ? maxmat[i][j] : maxSq;
+			}
+		}
+		return maxSq;
+	}
 
 }
---
+++ java-based/src/geeksforgeeks/company/amazon/RemoveMinElements.java
@@ -0,0 +1,45 @@
+/**
+ * Remove minimum elements.
+ * http://www.practice.geeksforgeeks.org/problem-page.php?pid=445
+ */
+package geeksforgeeks.company.amazon;
+
+public class RemoveMinElements {
+	public static void main(String[] args) {
+		int[] arr = { 4, 5, 100, 9, 10, 11, 12, 15, 200 };
+
+		removeMinEle(arr);
+	}
+
+	static void removeMinEle(int[] arr) {
+		boolean[][] ttable = new boolean[arr.length][arr.length];
+		int[][] mintable = new int[arr.length][arr.length];
+		int[][] maxtable = new int[arr.length][arr.length];
+
+		for (int l = 1; l <= arr.length; l++) {
+			for (int j = 0; j + l - 1 < arr.length; j++) {
+				if (l == 1) {
+					mintable[j][l + j - 1] = arr[j];
+					maxtable[j][l + j - 1] = arr[j];
+					ttable[j][l + j - 1] = true;
+				} else {
+					mintable[j][l + j - 1] = Math.min(mintable[j][l + j - 2],
+							mintable[j + 1][l + j - 1]);
+					maxtable[j][l + j - 1] = Math.max(maxtable[j][l + j - 2],
+							maxtable[j + 1][l + j - 1]);
+					if (2 * mintable[j][l + j - 1] > maxtable[j][l + j - 1])
+						ttable[j][l + j - 1] = true;
+				}
+			}
+		}
+
+		outer: for (int l = arr.length; l > 0; l--) {
+			for (int j = 0; j + l - 1 < arr.length; j++) {
+				if (ttable[j][l + j - 1]) {
+					System.out.print(arr.length-l);
+					break outer;
+				}
+			}
+		}
+	}
+}
