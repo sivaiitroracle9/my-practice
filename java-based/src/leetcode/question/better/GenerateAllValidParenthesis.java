514cd14ae63ba732cd865b4534f9ba351e9e16a1
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-04-08 11:40:44 +0800

    NQueens. final

---
+++ java-based/src/leetcode/question/better/GenerateAllValidParenthesis.java
@@ -0,0 +1,36 @@
+package leetcode.question.better;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateAllValidParenthesis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenerateAllValidParenthesis gV = new GenerateAllValidParenthesis();
+		for (String s : gV.genParenthesis(3)) {
+			System.out.print(s + ", ");
+		}
+	}
+
+	private List<String> genParenthesis(int n) {
+		List<String> result = new ArrayList<String>();
+		gen(result, 0, 0, n, "");
+		return result;
+	}
+
+	private void gen(List<String> result, int left, int right, int n,
+			String temp) {
+
+		if (left == right && left == n) {
+			result.add(temp);
+			return;
+		}
+
+		if (left + 1 <= n && right <= left + 1)
+			gen(result, left + 1, right, n, temp + '(');
+		if (right + 1 <= n && right+1 <= left)
+			gen(result, left, right + 1, n, temp + ')');
+
+	}
+}
---
+++ java-based/src/leetcode/question/better/NQueens.java
@@ -0,0 +1,60 @@
+package leetcode.question.better;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NQueens {
+
+	public static void main(String[] args) {
+		NQueens nq = new NQueens();
+		int N = 4;
+		List<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+		LinkedList<Integer> queen = new LinkedList<Integer>();
+		for(int i=0; i<N; i++) queen.add(-1);
+		nq.nQueens(result, N, 0, queen);
+
+		for (LinkedList<Integer> list : result) {
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (list.get(r) == c) {
+						System.out.print("Q");
+					} else
+						System.out.print("X");
+				}
+				System.out.println();
+			}
+			System.out.println("---------------------------------------");
+		}
+
+	}
+
+	private void nQueens(List<LinkedList<Integer>> result, int N, int row,
+			LinkedList<Integer> queen) {
+
+		if (row == N) {
+			LinkedList<Integer> finalQ = new LinkedList<Integer>();
+			finalQ.addAll(queen);
+			result.add(finalQ);
+			return;
+		}
+
+		for (int c = 0; c < N; c++) {
+			if (canPut(row, c, queen)) {
+				queen.set(row, c);
+				nQueens(result, N, row + 1, queen);
+				queen.set(row, -1);
+			}
+		}
+
+	}
+
+	private boolean canPut(int row, int col, List<Integer> queen) {
+
+		if (row - 1 >= 0 && (Math.abs(queen.get(row - 1) - col) == 2)
+				|| (row - 2 >= 0 && Math.abs(queen.get(row - 2) - col) == 1))
+			return false;
+
+		return true;
+	}
+}
