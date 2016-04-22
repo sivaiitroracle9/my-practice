1070aa94b89372a3758a0b6b21bf4dfcdc862cad
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-04-22 16:44:28 +0800

    Graph Algorithms.!!

--- java-based/src/algorithms/graph/Graph.java
+++ java-based/src/algorithms/graph/Graph.java
@@ -1,27 +1,256 @@
 package algorithms.graph;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph<T> {
-	
+
 	boolean isDirected;
-	
+
+	private List<Edge<T>> allEdges;
+	private Map<Long, Vertex<T>> allVertex;
+
 	Graph(boolean isDirected) {
+		allEdges = new ArrayList<Edge<T>>();
+		allVertex = new HashMap<Long, Vertex<T>>();
 		this.isDirected = isDirected;
 	}
-	
-	class Vertex<T> {
-		
-	}
-	
-	class Edge<T> {
-		
-		private Vertex<T> vertex1;
-		private Vertex<T> vertex2;
-		
-		Edge(Vertex<T> v1, Vertex<T> v2) {
-			this.vertex1 = v1;
-			this.vertex2 = v2;
-		}
-		
+
+	public List<Edge<T>> getAllEdges() {
+		return allEdges;
 	}
 
+	public void addEdge(long id1, long id2) {
+		addEdge(id1, id2, 0);
+	}
+
+	public void addEdge(long id1, long id2, int weight) {
+		Vertex<T> vertex1 = null;
+		if (allVertex.containsKey(id1)) {
+			vertex1 = allVertex.get(id1);
+		} else {
+			vertex1 = new Vertex<T>(id1);
+			allVertex.put(id1, vertex1);
+		}
+		Vertex<T> vertex2 = null;
+		if (allVertex.containsKey(id2)) {
+			vertex2 = allVertex.get(id2);
+		} else {
+			vertex2 = new Vertex<T>(id2);
+			allVertex.put(id2, vertex2);
+		}
+
+		Edge<T> edge = new Edge<T>(vertex1, vertex2, weight, isDirected);
+		allEdges.add(edge);
+		vertex1.addAdjacentVertex(edge, vertex2);
+		if (!isDirected) {
+			vertex2.addAdjacentVertex(edge, vertex1);
+		}
+	}
+
+	public Collection<Vertex<T>> getAllVertex() {
+		return allVertex.values();
+	}
+
+	public void setDataForVertex(long id, T data) {
+		if (allVertex.containsKey(id)) {
+			Vertex<T> vertex = allVertex.get(id);
+			vertex.setData(data);
+		}
+	}
+
+	public void addVertex(Vertex<T> vertex) {
+		if (allVertex.containsKey(vertex.getId())) {
+			return;
+		}
+		allVertex.put(vertex.getId(), vertex);
+		for (Edge<T> edge : vertex.getEdges()) {
+			allEdges.add(edge);
+		}
+	}
+
+	public Vertex<T> addSingleVertex(long id) {
+		if (allVertex.containsKey(id)) {
+			return allVertex.get(id);
+		}
+		Vertex<T> v = new Vertex<T>(id);
+		allVertex.put(id, v);
+		return v;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		for (Edge<T> edge : getAllEdges()) {
+			buffer.append(edge.getVertex1() + " " + edge.getVertex2() + " "
+					+ edge.getWeight());
+			buffer.append("\n");
+		}
+		return buffer.toString();
+	}
+}
+
+class Vertex<T> {
+	private long id;
+	private T data;
+	private List<Edge<T>> edges = new ArrayList<Edge<T>>();
+	private List<Vertex<T>> adjacentVertex = new ArrayList<Vertex<T>>();
+
+	public Vertex(long id) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void addAdjacentVertex(Edge<T> e, Vertex<T> v) {
+		edges.add(e);
+		adjacentVertex.add(v);
+	}
+
+	public List<Vertex<T>> getAdjacentVertexes() {
+		return adjacentVertex;
+	}
+
+	public List<Edge<T>> getEdges() {
+		return edges;
+	}
+
+	public int getDegree() {
+		return edges.size();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vertex other = (Vertex) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Vertex [id=" + id + ", data=" + data + "]";
+	}
+
+}
+
+class Edge<T> {
+
+	private Vertex<T> vertex1;
+	private Vertex<T> vertex2;
+	private long weight = 0;
+	private boolean directed = false;
+
+	Edge(Vertex<T> v1, Vertex<T> v2) {
+		this.vertex1 = v1;
+		this.vertex2 = v2;
+	}
+
+	Edge(Vertex<T> v1, Vertex<T> v2, int weight, boolean isDirected) {
+		this.vertex1 = v1;
+		this.vertex2 = v2;
+		this.weight = weight;
+		directed = isDirected;
+	}
+
+	public Vertex<T> getVertex1() {
+		return vertex1;
+	}
+
+	public void setVertex1(Vertex<T> vertex1) {
+		this.vertex1 = vertex1;
+	}
+
+	public Vertex<T> getVertex2() {
+		return vertex2;
+	}
+
+	public void setVertex2(Vertex<T> vertex2) {
+		this.vertex2 = vertex2;
+	}
+
+	public long getWeight() {
+		return weight;
+	}
+
+	public void setWeight(long weight) {
+		this.weight = weight;
+	}
+
+	public boolean isDirected() {
+		return directed;
+	}
+
+	public void setDirected(boolean directed) {
+		this.directed = directed;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((vertex1 == null) ? 0 : vertex1.hashCode());
+		result = prime * result + ((vertex2 == null) ? 0 : vertex2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (vertex1 == null) {
+			if (other.vertex1 != null)
+				return false;
+		} else if (!vertex1.equals(other.vertex1))
+			return false;
+		if (vertex2 == null) {
+			if (other.vertex2 != null)
+				return false;
+		} else if (!vertex2.equals(other.vertex2))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Edge [vertex1=" + vertex1 + ", vertex2=" + vertex2
+				+ ", weight=" + weight + ", directed=" + directed + "]";
+	}
 }
