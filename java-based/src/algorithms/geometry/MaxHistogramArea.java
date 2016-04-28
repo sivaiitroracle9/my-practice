1a2e4081498d8694708ea315a339fb44217d8425
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-04-28 19:00:34 +0800

    MaxHistogram Area :  Final commit for O(N).

---
+++ java-based/src/algorithms/geometry/MaxHistogramArea.java
@@ -0,0 +1,57 @@
+package algorithms.geometry;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaxHistogramArea {
+
+	public static void main(String[] args) {
+		int[] hist = { 2, 2, 2, 6, 1, 5, 4, 2, 2, 2, 2 };
+
+		int area = maxAreaRectangle(hist);
+		System.out.println(area);
+		assert area == 12;
+	}
+
+	private static int maxAreaRectangle(int[] hist) {
+
+		Deque<Integer> stack = new LinkedList<Integer>();
+		int max = 0;
+		int area;
+		int i = 0;
+		while (i < hist.length) {
+
+			if (stack.isEmpty() || hist[stack.peekFirst()] <= hist[i]) {
+				stack.offerFirst(i);
+				i++;
+			}
+
+			else {
+				int top = stack.pollFirst();
+
+				if (stack.isEmpty()) {
+					area = hist[top] * i;
+				} else {
+					area = hist[top] * (i - stack.peekFirst() - 1);
+				}
+
+				max = Math.max(area, max);
+			}
+
+		}
+
+		while (!stack.isEmpty()) {
+			int top = stack.pollFirst();
+
+			if (stack.isEmpty()) {
+				area = hist[top] * i;
+			} else {
+				area = hist[top] * (i - stack.peekFirst() - 1);
+			}
+
+			max = Math.max(max, area);
+		}
+		return max;
+	}
+
+}
