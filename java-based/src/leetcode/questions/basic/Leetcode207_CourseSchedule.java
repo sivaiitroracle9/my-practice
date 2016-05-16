a59074a9bec39f1314d40c2fb2c8f7588d667445
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-05-16 19:05:06 +0800

    Leetcode: Course-Schedule -- Topological Sort problems final commit.

---
+++ java-based/src/leetcode/questions/basic/Leetcode207_CourseSchedule.java
@@ -0,0 +1,72 @@
+package leetcode.questions.basic;
+/**
+ * Time Complexity : O(V+E)
+ * Space Conplexity: O(V)
+ * 
+ * https://leetcode.com/problems/course-schedule/
+ * 
+ * @author Siva Kumar Edupuganti
+ * @email sivaiitroracle9@gmail.com
+ * 
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Leetcode207_CourseSchedule {
+
+	public static void main(String[] args) {
+		Leetcode207_CourseSchedule lc = new Leetcode207_CourseSchedule();
+
+		int numCourses = 8;
+		int[][] prerequisites = { { 1, 0 }, { 2, 6 }, { 1, 7 }, { 6, 4 },
+				{ 7, 0 }, { 0, 5 } };
+		System.out.println(lc.canFinish(numCourses, prerequisites));
+	}
+
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+		Map<Integer, List<Integer>> prereq_courses = new HashMap<Integer, List<Integer>>();
+
+		for (int[] prereq : prerequisites) {
+			if (!prereq_courses.containsKey(prereq[1])) {
+				prereq_courses.put(prereq[1], new ArrayList<Integer>());
+			}
+			prereq_courses.get(prereq[1]).add(prereq[0]);
+		}
+
+		boolean[] visited = new boolean[numCourses];
+		boolean[] recursiveStack = new boolean[numCourses];
+		for (int node : prereq_courses.keySet()) {
+			if (isCycleExists(prereq_courses, node, visited, recursiveStack)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean isCycleExists(Map<Integer, List<Integer>> pre, int node,
+			boolean[] visited, boolean[] recursiveStack) {
+
+		if (!visited[node]) {
+			visited[node] = true;
+			recursiveStack[node] = true;
+			if (pre.get(node) != null) {
+				for (int child : pre.get(node)) {
+					if (!visited[child]
+							&& isCycleExists(pre, child, visited,
+									recursiveStack)) {
+						return true;
+					} else if (recursiveStack[child]) {
+						return true;
+					}
+				}
+			}
+		}
+		recursiveStack[node] = false;
+		return false;
+	}
+
+}
---
+++ java-based/src/leetcode/questions/basic/Leetcode210_CourseScheduleII.java
@@ -0,0 +1,105 @@
+package leetcode.questions.basic;
+
+/**
+ * Time Complexity : O(V+E)
+ * Space Conplexity: O(V)
+ * 
+ * https://leetcode.com/problems/course-schedule-ii/
+ * 
+ * @author Siva Kumar Edupuganti
+ * @email sivaiitroracle9@gmail.com
+ * 
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Leetcode210_CourseScheduleII {
+
+	public static void main(String[] args) {
+		Leetcode210_CourseScheduleII lc = new Leetcode210_CourseScheduleII();
+		int numCourses = 8;
+		int[][] prerequisites = { { 1, 0 }, { 2, 6 }, { 1, 7 }, { 6, 4 },
+				{ 7, 0 }, { 0, 5 } };
+		for (int i : lc.findOrder(numCourses, prerequisites)) {
+			System.out.print(i + ", ");
+		}
+	}
+
+	public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+		Map<Integer, List<Integer>> prereqmap = new HashMap<Integer, List<Integer>>();
+
+		for (int[] pre : prerequisites) {
+			if (!prereqmap.containsKey(pre[1]))
+				prereqmap.put(pre[1], new ArrayList<Integer>());
+			prereqmap.get(pre[1]).add(pre[0]);
+		}
+
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean[] visited = new boolean[numCourses];
+		boolean[] recursionStack = new boolean[numCourses];
+		for (int i = 0; i < numCourses; i++) {
+			if (!findOrder(prereqmap, i, visited, recursionStack, stack)) {
+				return null;
+			}
+		}
+
+		int[] res = new int[numCourses];
+		int i = 0;
+		while (!stack.isEmpty() && i < numCourses) {
+			res[i++] = stack.pop();
+		}
+		return res;
+	}
+
+	private boolean findOrder(Map<Integer, List<Integer>> premap, int node,
+			boolean[] visited, boolean[] rec_stack, Stack<Integer> result) {
+
+		if (!visited[node]) {
+			visited[node] = true;
+			rec_stack[node] = true;
+
+			if (premap.get(node) != null) {
+				for (int child : premap.get(node)) {
+					if (!visited[child]
+							&& !findOrder(premap, child, visited, rec_stack,
+									result)) {
+						return false;
+					} else if (rec_stack[child])
+						return false;
+				}
+			}
+			result.push(node);
+		}
+
+		rec_stack[node] = false;
+		return true;
+	}
+
+	private boolean isCycleExists(Map<Integer, List<Integer>> premap, int node,
+			boolean[] visited, boolean[] rec_stack) {
+
+		if (!visited[node]) {
+			visited[node] = true;
+			rec_stack[node] = true;
+
+			if (premap.get(node) != null) {
+				for (int child : premap.get(node)) {
+					if (!visited[child]
+							&& isCycleExists(premap, child, visited, rec_stack)) {
+						return true;
+					}
+
+					if (rec_stack[child])
+						return true;
+				}
+			}
+		}
+
+		rec_stack[node] = false;
+		return false;
+	}
+}
