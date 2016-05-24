32030cc1b1cb380f746d96b1fa31f51ec0883249
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-05-25 01:39:19 +0800

    Leetcode: Decode Ways DP

---
+++ java-based/src/leetcode/question/better/Leetcode32_LongestValidParenthesis.java
@@ -0,0 +1,32 @@
+package leetcode.question.better;
+
+public class Leetcode32_LongestValidParenthesis {
+
+	public static void main(String[] args) {
+		Leetcode32_LongestValidParenthesis lc = new Leetcode32_LongestValidParenthesis();
+		String s = "()(()";
+		System.out.println(lc.longestValidParentheses(s));
+	}
+
+	public int longestValidParentheses(String s) {
+		char[] sarr = s.toCharArray();
+		int left = 0, right = 0, max_length = 0;
+
+		for (int i = 0; i < sarr.length; i++) {
+			if (sarr[i] == '(') {
+				left++;
+			} else {
+				right++;
+			}
+
+			if (left - right < 0) {
+				left = 0;
+				right = 0;
+			}
+
+			if (max_length < 2 * right)
+				max_length = 2 * right;
+		}
+		return max_length;
+	}
+}
---
+++ java-based/src/leetcode/question/better/Leetcode91_DecodeWays.java
@@ -0,0 +1,35 @@
+package leetcode.question.better;
+
+public class Leetcode91_DecodeWays {
+
+	public static void main(String[] args) {
+		Leetcode91_DecodeWays lc = new Leetcode91_DecodeWays();
+		String s = "12";
+		System.out.println(lc.numDecodings(s));
+	}
+
+	public int numDecodings(String s) {
+		char[] csarr = s.toCharArray();
+		if (csarr.length == 0)
+			return 0;
+
+		int prev1 = 0, prev2 = 1, current = 0;
+		if (csarr[0] - '0' > 0 && csarr[0] - '0' <= 26)
+			prev1 = 1;
+
+		for (int i = 1; i < csarr.length; i++) {
+			int curr = csarr[i] - '0';
+
+			if (curr != 0)
+				current = current + prev1;
+
+			int prev = (csarr[i - 1] - '0') * 10 + curr;
+			if (prev <= 26 && prev >= 0)
+				current = current + prev2;
+
+			prev2 = prev1;
+			prev1 = current;
+		}
+		return prev1;
+	}
+}
