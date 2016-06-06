94f6747fd8d7cfcc29165aa2bcfa5d24343a17a9
sivaiitroracle9 <sivaiitroracle9@gmail.com>
2016-06-06 16:04:07 +0800

    Leetcode: Uber-Hard

---
+++ java-based/src/leetcode/company/dropbox/L290_WordPattern.java
@@ -0,0 +1,37 @@
+package leetcode.company.dropbox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L290_WordPattern {
+
+	public static void main(String[] args) {
+		String pattern = "abba";
+		String str = "dog dog dog dog";
+		System.out.println(wordPattern(pattern, str));
+	}
+
+	public static boolean wordPattern(String pattern, String str) {
+		String[] words = str.split(" ");
+
+		if (words.length != pattern.length())
+			return false;
+
+		Map<Character, String> char_word_map = new HashMap<Character, String>();
+		Map<String, Character> word_char_map = new HashMap<String, Character>();
+		for (int i = 0; i < pattern.length(); i++) {
+			if (!char_word_map.containsKey(pattern.charAt(i))
+					&& !word_char_map.containsKey(words[i])) {
+				char_word_map.put(pattern.charAt(i), words[i]);
+				word_char_map.put(words[i], pattern.charAt(i));
+			} else if ((!char_word_map.containsKey(pattern.charAt(i)) && word_char_map
+					.containsKey(words[i]))
+					|| (char_word_map.containsKey(pattern.charAt(i)) && !word_char_map
+							.containsKey(words[i]))
+					|| !char_word_map.get(pattern.charAt(i)).equals(words[i])
+					|| !word_char_map.get(words[i]).equals(pattern.charAt(i)))
+				return false;
+		}
+		return true;
+	}
+}
---
+++ java-based/src/leetcode/company/dropbox/L291_WordPatternII.java
@@ -0,0 +1,39 @@
+package leetcode.company.dropbox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L291_WordPatternII {
+	public static void main(String[] args) {
+		String pattern = "abba";
+		String str = "dogcatcatdog";
+		System.out.println(wordPatternMatch(pattern, str));
+	}
+
+	public static boolean wordPatternMatch(String pattern, String str) {
+		int N = pattern.length();
+		int L = str.length();
+		if (L % N != 0)
+			return false;
+		int wordlen = L / N;
+
+		Map<Character, String> char_word_map = new HashMap<Character, String>();
+		Map<String, Character> word_char_map = new HashMap<String, Character>();
+		for (int i = 0; i < N; i++) {
+			String word = str.substring(i * wordlen, (i + 1) * wordlen);
+			if (!char_word_map.containsKey(pattern.charAt(i))
+					&& !word_char_map.containsKey(word)) {
+				char_word_map.put(pattern.charAt(i), word);
+				word_char_map.put(word, pattern.charAt(i));
+			} else if ((!char_word_map.containsKey(pattern.charAt(i)) && word_char_map
+					.containsKey(word))
+					|| (char_word_map.containsKey(pattern.charAt(i)) && !word_char_map
+							.containsKey(word))
+					|| !char_word_map.get(pattern.charAt(i)).equals(word)
+					|| !word_char_map.get(word).equals(pattern.charAt(i)))
+				return false;
+		}
+		return true;
+
+	}
+}
